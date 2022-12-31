fbde94d641e7b90b7b8fb7a712975013a2bc6ba1
echen642 <98108241+echen642@users.noreply.github.com>
2022-12-31 15:28:50 -0600

    Takes in input until the user gives an empty input

--- Unscrambler.java
+++ Unscrambler.java
@@ -7,16 +7,10 @@ import java.io.*;
 
 // TODO: CREATE AN UNSCRAMBLER
 
-// TODO: GET A REFERENCE PAGE FOR WORDS (DONE)
-
-
-// TODO: IMPLEMENT A WAY TO INCLUDE INPUT FROM USER (DONE)
 class Unscrambler {
     public static void main(String[] args) throws IOException{
         Scanner scan = new Scanner(System.in);
         HashMap<Character, Integer> letterFreqDict = new HashMap<Character, Integer>();
-
-        // TODO: Convert all words in the file to a HashMap<Word, Map<Character, Integer>> (Done)
         File file = new File("words_alpha.txt");
         Scanner fScan = new Scanner(file);
 
@@ -44,35 +38,40 @@ class Unscrambler {
         fScan.close();
 
 
-        
+        // TODO: Implement a while loop to keep asking for scrambled words (Done)
         // This reads user input
-        System.out.print("Please type the scrambled word: ");
-        String userInput = scan.nextLine();
-        System.out.println("\n");
-        userInput = userInput.toUpperCase();
-        
-        char[] userInputArray = userInput.toCharArray();
-        for (Character c : userInputArray) {
-            System.out.println(c);
-            if (letterFreqDict.get(c) == null) {
-                letterFreqDict.put(c, 1);
-            } else {
-                letterFreqDict.put(c, letterFreqDict.get(c) + 1);
+        while (true) {
+            System.out.print("Please type the scrambled word: ");
+            letterFreqDict.clear();
+            String userInput = scan.nextLine();
+            userInput = userInput.toUpperCase();
+
+            if (userInput.equals("")) {
+                break;
             }
-        }
+            
+            char[] userInputArray = userInput.toCharArray();
 
-        // System.out.println(letterFreqDict);
-        ArrayList<String> possibleWords = new ArrayList<String>();
-        for (String s : bigMap.keySet()) {
-            // System.out.println(s);
-            if (bigMap.get(s).equals(letterFreqDict)) {
-                possibleWords.add(s);
+            for (Character c : userInputArray) {
+                if (letterFreqDict.get(c) == null) {
+                    letterFreqDict.put(c, 1);
+                } else {
+                    letterFreqDict.put(c, letterFreqDict.get(c) + 1);
+                }
             }
+            
+            // System.out.println(letterFreqDict);
+            ArrayList<String> possibleWords = new ArrayList<String>();
+            for (String s : bigMap.keySet()) {
+                // System.out.println(s);
+                if (bigMap.get(s).equals(letterFreqDict)) {
+                    possibleWords.add(s);
+                }
+            }
+    
+            System.out.println(possibleWords);
         }
-
-        System.out.println(possibleWords);
-        // TODO: READ THE WORDS_ALPHA TXT FILE USING FILEREADER
-
     }
+        
 }
 
