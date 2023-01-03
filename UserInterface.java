bc1a4a594a91abcc6b15efe205bd29e19bcfe398
echen642 <98108241+echen642@users.noreply.github.com>
2023-01-03 13:49:41 -0600

    Created the user interface using BorderLayout instead of GridBagLayout

--- UserInterface.java
+++ UserInterface.java
@@ -1,62 +1,72 @@
 import javax.swing.*;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.lang.reflect.GenericDeclaration;
+import java.awt.Color;
 import java.awt.GridBagConstraints;
+import java.awt.BorderLayout;
 
 
 public class UserInterface {
     public static void main(String args[]) {
-        JFrame frame = new JFrame("I am the title of this box");
-        JButton button1 = new JButton("Number 1");
-        JButton button2 = new JButton("Number 2");
-        JButton button3 = new JButton("Number 3");
-        JButton button4 = new JButton("Number 4");
-        JButton button5 = new JButton("Number 5");
+        JFrame frame = new JFrame("Word Unscrambler");
+        JButton button1 = new JButton("Enter");
 
-        JMenuBar menuBar = new JMenuBar();
-        JMenu fileButton = new JMenu("FILE");
-        JMenu helpButton = new JMenu("HELP");
+        // JMenuBar menuBar = new JMenuBar();
+        // JMenu fileButton = new JMenu("FILE");
+        // JMenu helpButton = new JMenu("HELP");
 
-        menuBar.add(fileButton);
-        menuBar.add(helpButton);
+        // menuBar.add(fileButton);
+        // menuBar.add(helpButton);
 
-        GridBagLayout grid = new GridBagLayout();
-        GridBagConstraints gbc = new GridBagConstraints();
+        // GridBagLayout grid = new GridBagLayout();
+        // GridBagConstraints gbc = new GridBagConstraints();
 
-        frame.setLayout(grid);
+        JPanel panel = new JPanel();
+        JLabel label = new JLabel("Enter Text:");
+        JTextField textField = new JTextField(10);
+        JLabel returnLabel = new JLabel();
+        
+        panel.add(label);
+        panel.add(textField);
+        panel.add(button1);
+        
+        frame.add(panel);
+        panel.setBackground(Color.CYAN);
+        
+
+
         // gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.anchor = GridBagConstraints.PAGE_START;
+        // gbc.anchor = GridBagConstraints.PAGE_START;
+        frame.add(BorderLayout.SOUTH, panel);
+        frame.add(BorderLayout.CENTER, returnLabel);
+        // gbc.anchor = GridBagConstraints.CENTER;
+        // gbc.gridx = 0;
+        // gbc.gridy = 0;    
+        // frame.add(button1, gbc);
 
-        frame.add(menuBar, gbc);
-        gbc.anchor = GridBagConstraints.CENTER;
-        gbc.gridx = 0;
-        gbc.gridy = 0;    
-        frame.add(button1, gbc);
+        // gbc.gridx = 1;
+        // gbc.gridy = 0;
+        // frame.add(button2, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        frame.add(button2, gbc);
+        // // gbc.fill = GridBagConstraints.HORIZONTAL;
+        // gbc.ipady = 20;
+        // gbc.gridx = 0;
+        // gbc.gridy = 1;
+        // frame.add(button3, gbc);
 
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.ipady = 20;
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        frame.add(button3, gbc);
+        // gbc.gridx = 1;
+        // gbc.gridy = 1;
+        // frame.add(button4, gbc);
 
-        gbc.gridx = 1;
-        gbc.gridy = 1;
-        frame.add(button4, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.gridwidth = 2;
-        frame.add(button5, gbc);
+        // gbc.gridx = 0;
+        // gbc.gridy = 2;
+        // gbc.gridwidth = 2;
+        // frame.add(button5, gbc);
     
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
 
-        // // frame.getContentPane().add(button);
-        // // frame.getContentPane().add(button1);
         frame.setVisible(true);
     }
 
