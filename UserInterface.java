4d2c3f333c01cb796f60a2e6c240fea7a2744302
echen642 <98108241+echen642@users.noreply.github.com>
2023-01-09 14:03:06 -0600

    User interface will allow the users to add a text file of their own to the default wordset

--- UserInterface.java
+++ UserInterface.java
@@ -1,9 +1,13 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.awt.Color;
 import java.awt.BorderLayout;
+import java.awt.Desktop;
+import java.io.IOException;
+
 
 
 public class UserInterface {
@@ -14,38 +18,75 @@ public class UserInterface {
     private JLabel label;
     private JTextField textField;
     private JLabel returnLabel;
+    private JMenuBar menuBar;
+    private JMenu menu;
+    private JMenuItem fileExplorerItem;
+    private JFileChooser fileChooser;
 
     public UserInterface() throws FileNotFoundException {
         unscrambler = new Unscrambler();
         unscrambler.setUp();
+        build();
+    }
+
+    private void build() {
         frame = new JFrame("Word Unscrambler");
         button1 = new JButton("Enter");
 
         panel = new JPanel();
         label = new JLabel("Enter Text:");
-        textField = new JTextField(31);     // Longest word in reference file is 31 letters
+        textField = new JTextField(unscrambler.getMaxLength());     // Longest word in default reference file is 31 letters
         returnLabel = new JLabel();
+        menuBar = new JMenuBar();
+        menu = new JMenu("File");
+        fileExplorerItem = new JMenuItem("Choose File");
+        fileChooser = new JFileChooser();
 
+        // Return Label Settings
         returnLabel.setHorizontalAlignment(SwingConstants.CENTER);
         returnLabel.setVerticalAlignment(SwingConstants.CENTER);
 
 
-
+        // Adding JComponents to/adjusting JPanel
         panel.add(label);
         panel.add(textField);
         panel.add(button1);
-        
-        frame.add(panel);
         panel.setBackground(Color.CYAN);
-        
+
+        // Adding JComponents to JFrame
+        frame.add(panel);
+        frame.add(BorderLayout.NORTH, menuBar);
         frame.add(BorderLayout.SOUTH, panel);
         frame.add(BorderLayout.CENTER, returnLabel);
+
+        // Adding MenuItem to Menu
+        menu.add(fileExplorerItem);
+        fileExplorerItem.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (fileChooser.showOpenDialog(fileExplorerItem) == JFileChooser.APPROVE_OPTION) {
+                    System.out.println("Your selected file is: " + new File(fileChooser.getSelectedFile().getAbsolutePath()));
+                    try {
+                        unscrambler.add(fileChooser.getSelectedFile());
+                    } catch (FileNotFoundException e1) {
+                        e1.printStackTrace();
+                    }
+                } else {
+                    System.out.println("The user has cancelled the operation.");
+                }
+                
+            }
+        });
+
+        // Adding Menu to MenuBar
+        menuBar.add(menu);
     
+        // Adjusting settings of JFrame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(600, 300);
-
         frame.setVisible(true);
 
+
+        // Adding functionality to button and textfield
         ActionListener processText = new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 processText();
