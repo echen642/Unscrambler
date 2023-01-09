cc57fd3b027916223bd34d6da6686be2866cf0b9
echen642 <98108241+echen642@users.noreply.github.com>
2023-01-09 14:02:22 -0600

    Unscrambler program now checks the maximum length of a word within a text file

--- Unscrambler.java
+++ Unscrambler.java
@@ -3,11 +3,14 @@ import java.util.HashMap;
 import java.util.Scanner;
 import java.io.*;
 
+
 // TODO: CREATE AN UNSCRAMBLER
 
 public class Unscrambler {
     private HashMap<Character, Integer> letterFreqDict;
     private HashMap<String, HashMap<Character, Integer>> bigMap;
+    private int maxLength;
+    private File currFile = new File("words_alpha.txt");
 
 
     Unscrambler() {
@@ -16,9 +19,59 @@ public class Unscrambler {
     }
 
     public void setUp() throws FileNotFoundException {
-        File file = new File("words_alpha.txt");
+        bigMap.clear();
+        Scanner fScan = new Scanner(currFile);
+
+        maxLength = checkMaxLength(currFile);
+        // ---- Creates our precomputed dictionary which we will rely on later ----
+        
+        while (fScan.hasNextLine() != false) {
+            String nextWord = fScan.nextLine();
+            nextWord = nextWord.toUpperCase();
+            char[] nextWordArray = nextWord.toCharArray();
+            HashMap<Character, Integer> wordMap = new HashMap<Character, Integer>();
+            for (Character c : nextWordArray) {
+                if (wordMap.get(c) == null) {
+                    wordMap.put(c, 1);
+                } else {
+                    wordMap.put(c, (wordMap.get(c) + 1));
+                }
+            }
+
+            bigMap.put(nextWord, wordMap);
+        }
+        fScan.close();
+    }
+
+    public void setUp(File file) throws FileNotFoundException {
+        bigMap.clear();
         Scanner fScan = new Scanner(file);
 
+        maxLength = checkMaxLength(file);
+        // ---- Creates our precomputed dictionary which we will rely on later ----
+        
+        while (fScan.hasNextLine() != false) {
+            String nextWord = fScan.nextLine();
+            nextWord = nextWord.toUpperCase();
+            char[] nextWordArray = nextWord.toCharArray();
+            HashMap<Character, Integer> wordMap = new HashMap<Character, Integer>();
+            for (Character c : nextWordArray) {
+                if (wordMap.get(c) == null) {
+                    wordMap.put(c, 1);
+                } else {
+                    wordMap.put(c, (wordMap.get(c) + 1));
+                }
+            }
+
+            bigMap.put(nextWord, wordMap);
+        }
+        fScan.close();
+    }
+
+
+    public void add(File file) throws FileNotFoundException {
+        Scanner fScan = new Scanner(file);
+        maxLength = Math.max(maxLength, checkMaxLength(file));
         // ---- Creates our precomputed dictionary which we will rely on later ----
         
         while (fScan.hasNextLine() != false) {
@@ -71,11 +124,38 @@ public class Unscrambler {
         return possibleWords.toString();
 
     }
-    public static void main(String[] args) throws IOException{
-        Unscrambler u = new Unscrambler();
-        u.setUp();
-        System.out.println(u.unscramble("naple  ").toString());
-    }
-        
-}
 
+    /**
+     * Given a reference file, will figure out the length of the longest word in the
+     * reference file.
+     * 
+     * @param file
+     * @return
+     * @throws FileNotFoundException
+     */
+    private int checkMaxLength(File file) throws FileNotFoundException{
+        Scanner fScan = new Scanner(file);
+        int maxWordLength = 0;
+
+        // ---- Creates our precomputed dictionary which we will rely on later ----
+        
+        while (fScan.hasNextLine() != false) {
+            String nextWord = fScan.nextLine();
+            if (maxWordLength == 0) {
+                maxWordLength = nextWord.length();
+            } else {
+                maxWordLength = Math.max(maxWordLength, nextWord.length());
+            }
+        }
+        fScan.close();
+        return maxWordLength;
+    }
+
+    public int getMaxLength() {
+        return maxLength;
+    }
+    public static void main(String[] args) throws IOException{
+        Unscrambler unscrambler = new Unscrambler();
+        System.out.println(unscrambler.checkMaxLength(new File("words_alpha.txt")));
+    }
+}
